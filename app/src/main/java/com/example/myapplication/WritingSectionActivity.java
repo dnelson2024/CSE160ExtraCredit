4f0bfacd7936ea44a660a34000a9d489d18943be
Devyn06 <devyn.lewis@csedge.org>
2025-03-29 14:46:55 -0400

    WS: Answer checking completed

--- app/src/main/java/com/example/myapplication/WritingSectionActivity.java
+++ app/src/main/java/com/example/myapplication/WritingSectionActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +13,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import org.w3c.dom.Text;
 
 public class WritingSectionActivity extends AppCompatActivity {
     TextView text;
+    TextInputEditText textbox;
     @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
@@ -38,6 +42,7 @@ public class WritingSectionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_writing_section);
 
         text = (TextView) findViewById(R.id.textView4);
+        textbox = (TextInputEditText) findViewById(R.id.TextBox);
         String questions = "To the right";
         String answers = "a la derecha";
         text.setText("To the right");
@@ -54,10 +59,39 @@ public class WritingSectionActivity extends AppCompatActivity {
 //
 //        String randString = "" + (int)((Math.random()*10)+2);
 //        btn.setText(randString);
+        String question = "" + text.getText();
+        int ansLength = 0;
+        String key = "a la derecha";
+        String ans = "";
+        String ansNS = "";
+        int count = 0;
+
         Log.d("Success", "Text Changed");
-        text.setText("Welp");
+        ans = "" + textbox.getText();
+        ansNS = ans.replace(" ", "");
+//        ansLength = ansNs.length();
 
-    }
+        if (ans.length() == key.length()) {
+            for (int i = 0; i < ans.length(); i++) {
+                if (ans.toLowerCase().charAt(i) == key.toLowerCase().charAt(i)) {
+                    count += 1;
+                    Log.d("Success", "" + count);
+                }
+            }
+            if(count == key.length()){
+                textbox.setTextColor(Color.rgb(0, 230, 0));
+            }
+            else{
+                textbox.setTextColor(Color.RED);
+            }
+
+        }
+
+        else{
+            textbox.setTextColor(Color.RED);
+        }
 
 
+
+}
 }
